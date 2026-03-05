03fea551a21819c089e439b3e93168a6f328e328
cassen999 <cassenpt@gmail.com>
2026-03-05 12:40:06 -0700

    created basic card and layout in intervals page

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,5 +1,11 @@
-const ControlPanel = () => {
+import { Card } from 'primereact/card';
 
+const ControlPanel = () => {
+  return (
+    <Card title='Controls'>
+      controls content
+    </Card>
+  );
 }
 
 export default ControlPanel;
--- src/pages/Intervals.tsx
+++ src/pages/Intervals.tsx
@@ -2,6 +2,7 @@ import Fretboard from "../components/Fretboard";
 import { createInterval } from "../helpers/createInterval";
 import { useControls } from "../contexts/ControlsContext";
 import { useMemo } from "react";
+import ControlPanel from "../components/ControlPanel";
 
 export const Intervals = () => {
   const { interval = 2, setInterval } = useControls();
@@ -23,19 +24,23 @@ export const Intervals = () => {
     () => createInterval({ interval: interval }),
     [interval],
   );
+  
   return (
     <div className="intervals-container">
       <h1 className="page-title">Intervals</h1>
       <div>Root note is pink, interval note is purple, unison is teal</div>
-      <div>
+      {/* <div>
         {([2, 3, 4, 5, 6, 7, 8] as const).map((i) => (
           <button key={i} onClick={() => setInterval && setInterval(i)}>
             {i}
           </button>
         ))}
-      </div>
+      </div> */}
 
-      <div className="page-section">
+      <div className="intervals-page-section">
+        <div className="page-subsection">
+          <ControlPanel />
+        </div>
         <div className="page-subsection">
           <h2>{interval + intervalSuffix(interval)}</h2>
           <Fretboard coords={coords} numFrets={6} />
