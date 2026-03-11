f7d3f9eff32fa0b7090d2654cf82671e2e83a68a
cassen999 <cassenpt@gmail.com>
2026-03-11 16:35:39 -0600

    switched to hash router so gh pages doesnt shit itself trying to do the basic thing is was created to do

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { PrimeReactProvider } from "primereact/api";
-import App from "./App.tsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
