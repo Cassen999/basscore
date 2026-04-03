5bf1f995de4bcac5c3bea2aad420f7ee17fb389a
cassen999 <cassenpt@gmail.com>
2026-04-03 12:32:41 -0600

    basic home page started

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
+import { Tooltip } from "primereact/tooltip";
 import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
@@ -44,7 +45,9 @@ export const Home = () => {
     const footer = (
       <Button label="Get Started" onClick={() => nav("/intervals")} />
     );
-    const content = <p>Learn the different fretboard intervals and their shapes.</p>;
+    const content = (
+      <p>Learn the different fretboard intervals and their shapes.</p>
+    );
 
     return (
       <Card title="Intervals" header={header} footer={footer}>
@@ -55,12 +58,55 @@ export const Home = () => {
 
   return (
     <div className="home-container">
-      BASSCORE Home Page
-      <div className="offerings-cards">
+      <section className="hero">
+        <div className="hero-content">
+          <h1 className="hero-title">
+            Don't be a baby, learn bass guitar with BASSCORE
+          </h1>
+          <div className="hero-subtitle">
+            <p className="hero-subtitle-text">
+              Join these famous bassists who also got gud with BASSCORE:
+            </p>
+            <ul className="hero-list">
+              <li>
+                <Tooltip
+                  target=".jaco"
+                  content="Definitely a lie but he for sure would have."
+                  mouseTrack
+                  mouseTrackLeft={10}
+                />
+                <p className="underline jaco">Jaco Pastorius,</p>
+              </li>
+              <li>
+                <Tooltip
+                  target=".flea"
+                  content="Also a lie. Honestly he probably wouldn't."
+                  mouseTrack
+                  mouseTrackLeft={10}
+                />
+                <p className="underline flea">Flea,</p>
+              </li>
+              <li>
+                <span className='li-one-line'>
+                  <p>and&nbsp;</p>
+                  <Tooltip
+                    target=".cassen"
+                    content="Not famous but he made this thing."
+                    mouseTrack
+                    mouseTrackLeft={10}
+                  />
+                  <p className="underline cassen">Cassen Gerber</p>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className="offerings-cards">
         {metronomeCard()}
         {scalesCard()}
         {intervalsCard()}
-      </div>
+      </section>
     </div>
   );
 };
