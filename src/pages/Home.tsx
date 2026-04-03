4e08e8aa16d3055f6854490d65211261c5c20988
cassen999 <cassenpt@gmail.com>
2026-04-03 11:43:45 -0600

    homepage cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,66 @@
+import { Button } from "primereact/button";
+import { Card } from "primereact/card";
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
+  const nav = useNavigate();
+  const metronomeCard = () => {
+    const header = <img src="images/metronome-image.png" />;
+    const footer = (
+      <Button label="Get Started" onClick={() => nav("/metronome")} />
+    );
+    const content = (
+      <p>
+        Practice your rhythm by choosing your own subdivision and beats per
+        minute
+      </p>
+    );
+
+    return (
+      <Card title="Metronome" header={header} footer={footer}>
+        {content}
+      </Card>
+    );
+  };
+
+  const scalesCard = () => {
+    const header = <img src="images/scale-image.png" />;
+    const footer = (
+      <Button label="Get Started" onClick={() => nav("/scales")} />
+    );
+    const content = (
+      <p>Learn your different scale modes applied to the fretboard.</p>
+    );
+
+    return (
+      <Card title="Scales" header={header} footer={footer}>
+        {content}
+      </Card>
+    );
+  };
+
+  const intervalsCard = () => {
+    const header = <img src="images/interval-image.png" />;
+    const footer = (
+      <Button label="Get Started" onClick={() => nav("/intervals")} />
+    );
+    const content = <p>Learn the different fretboard intervals and their shapes.</p>;
+
+    return (
+      <Card title="Intervals" header={header} footer={footer}>
+        {content}
+      </Card>
+    );
+  };
+
   return (
-    <div className='home-container'>
+    <div className="home-container">
       BASSCORE Home Page
+      <div className="offerings-cards">
+        {metronomeCard()}
+        {scalesCard()}
+        {intervalsCard()}
+      </div>
     </div>
   );
-}
+};
